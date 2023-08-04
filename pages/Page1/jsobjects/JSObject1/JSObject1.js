238e41ea38b0e1b2663042665cb04b983387c230
anandi <anandi@appsmith.com>
2023-08-04 10:50:53 +0000

    System generated commit, initial commit

---
+++ pages/Page1/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,25 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+	  console.log('sfdsfdsf')
+	},
+		myFun12 () {
+	  console.log('sfdsfdsf')
+	},
+		myFun13 () {
+	  console.log('sfdsfdsf')
+	},
+			myFun43 () {
+	  console.log('sfdsfdsf')
+	},
+	myFun413 () {
+	  showAlert('afsdsa')
+	},
+	async myFun2 () {
+		
+		showAlert('sdfdsf')
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
